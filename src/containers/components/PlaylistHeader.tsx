7cd56437243b84e87c6f90b6ba758058ee2cdbca
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-04-12 23:05:19 -0300

    header css

--- src/containers/components/PlaylistHeader.tsx
+++ src/containers/components/PlaylistHeader.tsx
@@ -1,9 +1,23 @@
-const PlaylistHeader = () => {
+import playlistCover from '../../assets/images/cover/playlist.png'
+
+type HeaderProps = {
+    title: string;
+    subtitle: string;
+}
+
+
+const PlaylistHeader = ( { title, subtitle }: HeaderProps) => {
     return (
-        <div>
-            <h3>
-                PlaylistHeader
-            </h3>
+        <div className="header-container">
+            <div className="header-background"/>
+            <div className="header-content">
+                <img src={playlistCover} alt="playlist cover" className="header-cover" />
+
+                <div className="info">
+                    <h2>{title}</h2>
+                    <h5>{subtitle}</h5>
+                </div>
+            </div>
         </div>
     )   
 }
